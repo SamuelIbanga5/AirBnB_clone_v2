3dc07bec542ec27d072216d7ec4929e894954057
Samuel Ibanga <ibangajnr27@gmail.com>
2023-03-21 21:54:23 +0000

    installed models package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 from setuptools import setup, find_packages
 
-setup(name="models", version=1.0, packages=find_packages())
+setup(name="models", version=2.0, packages=find_packages())
