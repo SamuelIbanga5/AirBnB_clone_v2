f257d7ab31dbf7cd49c43b7997ddbbaad771bc87
Samuel Ibanga <ibangajnr27@gmail.com>
2023-03-22 13:37:04 +0000

    DBStorage - States and Cities

--- models/city.py
+++ models/city.py
@@ -9,5 +9,5 @@ class City(BaseModel, Base):
     name = Column(String(128),
             nullable=False)
     state_id = Column(String(60),
-            nullable=False,
-            ForeignKey('states.id'))
+            ForeignKey('states.id'),
+            nullable=False)
--- setup_env_var.sh
+++ setup_env_var.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Sets up environment variables for project development.
-export HBNB_ENV='dev'
+export HBNB_ENV='dev':'test'
 export HBNB_MYSQL_USER='hbnb_dev'
 export HBNB_MYSQL_PWD= 'hbnb_dev_pwd'
 export HBNB_MYSQL_HOST= 'localhost'
 export HBNB_MYSQL_DB= 'hbnb_dev_db'
-export HBNB_TYPE_STORAGE= 'DBStorage'
+export HBNB_TYPE_STORAGE= 'DBStorage':'file'
