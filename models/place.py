7ed45bd3c4acbdda121507eb7f9553369cdc5f47
Samuel Ibanga <ibangajnr27@gmail.com>
2023-03-23 20:28:52 +0000

    fix code errors

--- models/place.py
+++ models/place.py
@@ -5,6 +5,20 @@ from models import storage_type
 from models.review import Review
 from sqlalchemy import Column, String, Integer, ForeignKey, Float
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import Table
+
+
+if storage_type == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True,
+                                 nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True,
+                                 nullable=False)
+                          )
 
 
 class Place(BaseModel, Base):
@@ -47,10 +61,6 @@ class Place(BaseModel, Base):
         longitude = Column(
                 Float,
                 nullable=True)
-        reviews = relationship(
-                'Review',
-                backref='place',
-                cascade="all, delete, delete-orphan")
     else:
         city_id = ""
         user_id = ""
--- models/state.py
+++ models/state.py
@@ -4,15 +4,33 @@ from models.base_model import BaseModel, Base
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models import storage_type
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(
-            String(128),
-            nullable=False)
-    cities = relationship(
-            'City',
-            backref='state',
-            cascade='all, delete, delete-orphan')
+    if storage_type == 'db':
+        name = Column(
+                String(128),
+                nullable=False)
+        cities = relationship(
+                'City',
+                backref='state',
+                cascade='all, delete, delete-orphan')
+    else:
+        name = ''
+
+        @property
+        def cities(self):
+            """returns the list of City instances with state_id equals
+            to the curent State.id.
+            """
+            from models import storage
+            lst = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    related_cities.append(city)
+            return related_cities
+
